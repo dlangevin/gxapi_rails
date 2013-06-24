243885cd1befb772222089810262a1ccb3496b68
Dan Langevin <dan.langevin@lifebooker.com>
2013-06-24 15:53:31 -0400

    Getting engine.rb to work in Rails 2

--- lib/moona/engine.rb
+++ lib/moona/engine.rb
@@ -1,9 +1,15 @@
-module Moona
-  class Engine < Rails::Engine
-    config.after_initialize do
-      Moona.cache = Rails.cache
-      # add our helper
-      ApplicationHelper.send(:include, MoonaHelper)
+if Rails::VERSION::MAJOR >= 3  
+  module Moona
+    class Engine < Rails::Engine
+      config.after_initialize do
+        Moona.cache = Rails.cache
+        # add our helper
+        ApplicationHelper.send(:include, MoonaHelper)
+      end
     end
+  end
+else
+  Rails.configuration.after_initialize do
+    ApplicationHelper.send(:include, MoonaHelper)
   end
 end
